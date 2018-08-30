aeb601edaae2ff7fc0c76379aef15235412553ed
ananyarane <42105274+ananyarane@users.noreply.github.com>
2018-08-30 10:44:19 +0530

    update 0048

--- Pset - 0048/sumprimes.py
+++ Pset - 0048/sumprimes.py
@@ -2,19 +2,20 @@ sum = 0
 
 def isprime(x):
     cnt = 0
-    for i in range(1,x+1):
+    h = int(x**0.5)
+    for i in range(2,h+1):
         if(x%i == 0):
-            cnt = cnt + 1
+            cnt  = cnt+ 1
 
-    if cnt == 2:
+    if cnt == 0:
         return True
     else:
         return False
 
 
 for i in range(1,2000000):
-    if isprime(i):
-        print (i)
-        sum = sum + i
+    if i%2!=0:
+        if isprime(i):
+            sum = sum + i
 
 print (str(sum))
