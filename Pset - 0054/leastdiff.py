2025831758dc8e5086a05a85596606c55626fb1f
ananyarane <42105274+ananyarane@users.noreply.github.com>
2018-08-31 11:48:20 +0530

    Updating 0054

--- Pset - 0054/leastdiff.py
+++ Pset - 0054/leastdiff.py
@@ -1,21 +1,22 @@
 n = []
-
+import math
 print("Enter a list of 10 numbers")
 for i in range (10):
     x = int(input())
     n.append(int(x))
 
-diff = n[1]-n[0]
-a = 0
-b = 0
+diff = abs((n[1]-n[0]))
+a = n[0]
+b = n[1]
 
-print (*n, sep = " ")
+
 
 for i in range(9):
-    if n[i+1]-n[i] < diff:
-        diff = n[i+1]-n[i]
+    if abs(n[i+1]-n[i]) < diff:
+        diff = abs((n[i+1]-n[i]))
         a = n[i]
         b = n[i+1]
 
+print()
 print(a)
 print(b)
