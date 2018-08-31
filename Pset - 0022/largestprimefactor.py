2414133ef96302e369a01a25c8cb15fab2ed89cd
ananyarane <42105274+ananyarane@users.noreply.github.com>
2018-08-31 11:47:48 +0530

    Updating 0022

--- Pset - 0022/largestprimefactor.py
+++ Pset - 0022/largestprimefactor.py
@@ -3,17 +3,17 @@ lf = 1
 
 def isprime(x):
     cnt = 0
-    for i in range(1,x+1):
+    for i in range(2,int(x**0.5)+1):
         if(x%i == 0):
             cnt = cnt + 1
 
-    if cnt == 2:
+    if cnt == 0:
         return True
     else:
         return False
 
 
-for i in range(1,n+1):
+for i in range(1,int(n**0.5)+1):
     if(n%i==0):
         flag = isprime(i)
         if flag:
