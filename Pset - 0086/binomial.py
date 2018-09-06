42ccb6d600020cb5fd6c4132cf53f8d7508343fa
ananyarane <42105274+ananyarane@users.noreply.github.com>
2018-09-06 21:18:15 +0530

    update 0086 - with comments

--- Pset - 0086/binomial.py
+++ Pset - 0086/binomial.py
@@ -1,4 +1,4 @@
-a = [1]
+a = [1] #take the first element to be 1
 b = []
 x = 1
 
@@ -6,28 +6,35 @@ print("Enter a number")
 n = int(input())
 
 if n == 0 :
-	print("0")
+	print("0") #if zero, answer is zero
 
 else:
 	
 	while x<=n:
-		a.insert(0,0)
-		a.append(0)
-		del b[:]
+		a.insert(0,0) #insert a 0 at the beginning of the list
+		a.append(0) #insert a 0 at the end of the list
+		del b[:] #empty the elements of list b
+
 		for i in range(len(a)-1):
 			j = i + 1
-			b.append(a[i] + a[j])
+			b.append(a[i] + a[j]) #to list b add elements of list a
+			''' for example:
+			a = [1,1]
+			a after inserting and appending = [0,1,1,0]
+			add the 0th element to the 1st element, 1st to 2nd and so on, because 110+011 = 121 and so the subsequent terms can be found like that.
+			'''
 
 		for i in range(0,len(b)):
-			a[i] = b[i]
+			a[i] = b[i] #reassign them to a, to loop it
 		
-		a.remove(0)
+		a.remove(0) #remove all the zeros
 		x = x + 1
 
 	x = n
 	y = 0
 	i = 0
 
+	#printing
 	print("(x+y)^"+str(n)+" = ",end='')
 	while x >= 0:
 		if x == 0:
